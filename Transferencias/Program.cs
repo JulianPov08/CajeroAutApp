bf249168d9d1d32218da3bff1577560cb3c815a1
Oscar Julian Poveda <oscarj140@hotmail.com>
2024-08-02 00:46:16 -0500

    Realizo el commit de Transferencias(dentro de la rama Funcion_Transferencia

---
+++ Transferencias/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Transferencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al Cajero CriptoMoney");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión Realizado");
+                Transfer();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión fallido");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Ingrese su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Ingrese su clave: ");
+            string password = Console.ReadLine();
+            return username == "Nequi" && password == "0809";
+        }
+
+        static void Transfer()
+        {
+            Console.Write("Ingrese la cantidad que quieres transferir: ");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Ingrese el número de cuenta del destinatario: ");
+            string accountNumber = Console.ReadLine();
+            
+            Console.WriteLine($"Has echo un transferido {amount:C} a la cuenta {accountNumber}.");
+        }
+    }
+}
