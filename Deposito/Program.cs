4d47ff044e5b7f53290036df79f012f08cb9840d
Oscar Julian Poveda <oscarj140@hotmail.com>
2024-08-02 00:37:39 -0500

    se realizo el commic de deposito (dentro la rama de Funcion_Deposito)

---
+++ Deposito/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Deposito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al Cajero CriptoMoney");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión Realizado");
+                Deposit();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión fallido");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Ingrese su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Ingrese su clave: ");
+            string password = Console.ReadLine();
+            return username == "Nequi" && password == "0809";
+        }
+
+        static void Deposit()
+        {
+            Console.Write("Ingrese la cantidad que vas a depositar: ");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            
+            Console.WriteLine($"Has depositado {amount:C}.");
+        }
+    }
+}
