5f738f9b46a32966f140c58b691929a4058a735f
Oscar Julian Poveda <oscarj140@hotmail.com>
2024-08-02 00:32:24 -0500

    Se Realizo commic de rerito (en la rama funcion_rerito).

---
+++ Retiro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Retiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al Cajero CriptoMoney");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión Realizado");
+                Withdraw();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión fallido");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Ingrese su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Ingrese su clave ");
+            string password = Console.ReadLine();
+            return username == "Nequi" && password == "0809";
+        }
+
+        static void Withdraw()
+        {
+            Console.Write("Ingrese la cantidad que quieres retirar: ");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+           
+            Console.WriteLine($"Has realizado un retiro {amount:C}.");
+        }
+    }
+}
