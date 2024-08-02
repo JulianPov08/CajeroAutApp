ba004802194b1eb49a61a6049b07bd2cf5cbab4d
Oscar Julian Poveda <oscarj140@hotmail.com>
2024-08-02 00:22:53 -0500

    Se Realiza El Inicio_Sesion (CON EL COMMIC FUNCION_INCIIOSESION).

---
+++ Iniciando_Sesion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al Cajero CriptoMoney");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión Realizado");
+                
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión fallido");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Ingrese su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Ingrese su clave ");
+            string password = Console.ReadLine();
+            
+            return username == " Nequi" && password == "0809"; 
+        }
+    }
+}
