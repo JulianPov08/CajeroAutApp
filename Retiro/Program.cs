1a2aabe6b4a3af4c0cf5a1824935765ad8519fd9
Oscar Julian Poveda <oscarj140@hotmail.com>
2024-08-02 15:30:24 -0500

    Se acaba realizar el cambio de codigo integrando orientada objetos y hago el commit en la rama Funcion_Retiro

--- Retiro/Program.cs
+++ Retiro/Program.cs
@@ -7,34 +7,44 @@ namespace Retiro
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenido al Cajero CriptoMoney");
-            if (Login())
+
+            Cajero cajero = new Cajero();
+
+            if (cajero.Login())
             {
-                Console.WriteLine("Inicio de sesión Realizado");
-                Withdraw();
+                Console.WriteLine("Inicio de sesión realizado");
+                cajero.Withdraw();
             }
             else
             {
                 Console.WriteLine("Inicio de sesión fallido");
             }
         }
+    }
 
-        static bool Login()
+    class Cajero
+    {
+        private string usuario = "Nequi";
+        private string clave = "0809";
+
+        public bool Login()
         {
             Console.Write("Ingrese su usuario: ");
             string username = Console.ReadLine();
-            Console.Write("Ingrese su clave ");
+            Console.Write("Ingrese su clave: ");
             string password = Console.ReadLine();
-            return username == "Nequi" && password == "0809";
+            return username == usuario && password == clave;
         }
 
-        static void Withdraw()
+        public void Withdraw()
         {
             Console.Write("Ingrese la cantidad que quieres retirar: ");
             decimal amount = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine($"Has realizado un retiro {amount:C}.");
+            Console.WriteLine($"Has realizado un retiro de {amount:C}.");
 
             Console.ReadKey();
         }
     }
 }
+
