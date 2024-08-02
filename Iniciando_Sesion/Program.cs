0d3de3449d9829811334cc8fcf7422bbfc3f8f33
Oscar Julian Poveda <oscarj140@hotmail.com>
2024-08-02 15:03:01 -0500

    Se Realiza en commit se sube nuevo codigo orientada a objetos !

--- Iniciando_Sesion/Program.cs
+++ Iniciando_Sesion/Program.cs
@@ -1,34 +1,61 @@
 ﻿using System;
 
-namespace Iniciando_Sesion 
+using System;
+
+namespace Iniciando_Sesion
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CajeroAutomatico cajero = new CajeroAutomatico();
+            cajero.Iniciar();
+        }
+    }
+
+    class CajeroAutomatico
+    {
+        private Usuario usuario;
+
+        public CajeroAutomatico()
+        {
+            usuario = new Usuario("Nequi", "0809");
+        }
+
+        public void Iniciar()
+        {
             Console.WriteLine("Bienvenido al Cajero CriptoMoney");
-            if (Login())
+            if (usuario.IniciarSesion())
             {
                 Console.WriteLine("Inicio de sesión Realizado");
-
             }
             else
             {
                 Console.WriteLine("Inicio de sesión fallido");
-
             }
             Console.ReadKey();
         }
+    }
 
-        static bool Login()
+    class Usuario
+    {
+        private string Username { get; set; }
+        private string Password { get; set; }
+
+        public Usuario(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool IniciarSesion()
         {
             Console.Write("Ingrese su usuario: ");
-            string username = Console.ReadLine();
-            Console.Write("Ingrese su clave ");
-            string password = Console.ReadLine();
-            
-            return username == "Nequi" && password == "0809";
-            Console.ReadKey();
+            string usernameInput = Console.ReadLine();
+            Console.Write("Ingrese su clave: ");
+            string passwordInput = Console.ReadLine();
+
+            return Username == usernameInput && Password == passwordInput;
         }
     }
 }
